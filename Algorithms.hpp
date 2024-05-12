331a6d7c66d702648310a58a272f60f1c38b135d
Gal Hillel <Galh2011@gmail.com>
2024-05-12 18:28:57 +0300

    Finish header file

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -1,3 +1,4 @@
+// Id: 211696521 Mail: galh2011@icloud.com
 #ifndef ALGORITHMS_HPP
 #define ALGORITHMS_HPP
 
@@ -12,9 +13,9 @@ namespace ariel
     public:
         static bool isConnected(const Graph &g);
         static std::string shortestPath(const Graph &g, int start, int end);
-        static std::string isContainsCycle(const Graph &g); //fix  
-        static std::string isBipartite(const Graph &g); // fix
-        static void negativeCycle(const Graph &g);
+        static std::string isContainsCycle(const Graph &g);
+        static std::string isBipartite(const Graph &g);
+        static std::string negativeCycle(const Graph &g);
 
     private:
         static bool dfs(const std::vector<std::vector<int>> &adjacencyMatrix, std::vector<bool> &visited, int start);
